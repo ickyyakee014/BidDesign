70bf6f9f90826d6b7d1bb8e570fc97f5ac735cb1
Nickyboy <Nickyboy@Nickys-MacBook-Pro.local>
2014-08-13 15:01:40 -0600

    Initial Commit

---
+++ BidDesign/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BidDesign
+//
+//  Created by Nickyboy on 2014-08-13.
+//  Copyright (c) 2014 NickyLAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BidDesign/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BidDesign
+//
+//  Created by Nickyboy on 2014-08-13.
+//  Copyright (c) 2014 NickyLAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
